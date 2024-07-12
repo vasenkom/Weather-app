2b473bbf3ba655f88fee03362bd165785004a1ad
vasenkom <m.vasenko@yandex.ru>
2024-07-12 16:31:49 +0200

    complete addRightPartDOM() and setRightPartDOMContent() functions

--- src/rightDOM.js
+++ src/rightDOM.js
@@ -1,4 +1,5 @@
 import { fetchNextThreeDaysData } from "./getDataAPI";
+import { fetchCurrentData } from "./getDataAPI.js";
 import { getCurrentAndNextDays } from "./date";
 import { addImage } from "./addImg";
 
@@ -13,11 +14,11 @@ export function addRightPartDOM() {
   thisWeekP.classList.add("thisWeekP");
   thisWeekP.textContent = "Next three days"; // permanent so add here
 
-  const weekDIV = document.createElement("div");
-  weekDIV.classList.add("weekDIV");
-
   rightDOM.appendChild(locationP);
   rightDOM.appendChild(thisWeekP);
+
+  const weekDIV = document.createElement("div");
+  weekDIV.classList.add("weekDIV");
 
   //   Weather card 1
   const card1 = document.createElement("div");
@@ -85,7 +86,93 @@ export function addRightPartDOM() {
   card3.appendChild(card3WeatherImg);
   weekDIV.append(card3);
 
+  const todayHighlightsP = document.createElement("p");
+  todayHighlightsP.classList.add("todayHighlightsP");
+
+  const todayHighlightsDIV = document.createElement("div");
+  todayHighlightsDIV.classList.add("todayHighlightsDIV");
+
+  // UV card
+  const cardUV = document.createElement("div");
+  cardUV.classList.add("todayHighlightsCardUV");
+  const cardUVp = document.createElement("p");
+  cardUVp.classList.add("todayHighlightsTitle");
+  cardUVp.textContent = "UV Index";
+  const cardUVvalue = document.createElement("p");
+  cardUVvalue.classList.add("todayHighlightsValue");
+  const sunscreenReminder = document.createElement("p");
+  sunscreenReminder.classList.add("sunscreenReminder");
+  cardUV.appendChild(cardUVp);
+  cardUV.appendChild(cardUVvalue);
+  cardUV.appendChild(sunscreenReminder);
+  todayHighlightsDIV.appendChild(cardUV);
+
+  // Feels like
+  const cardFeelsLike = document.createElement("div");
+  cardFeelsLike.classList.add("todayHighlightsCard");
+  const cardFeelsLikep = document.createElement("p");
+  cardFeelsLikep.classList.add("todayHighlightsTitle");
+  cardFeelsLikep.textContent = "Feels like..";
+  const feelsLikeValue = document.createElement("p");
+  feelsLikeValue.classList.add("todayHighlightsValue");
+  cardFeelsLike.appendChild(cardFeelsLikep);
+  cardFeelsLike.appendChild(feelsLikeValue);
+  todayHighlightsDIV.appendChild(cardFeelsLike);
+
+  // Pressure
+  const cardPressure = document.createElement("div");
+  cardPressure.classList.add("todayHighlightsCard");
+  const cardPressureP = document.createElement("p");
+  cardPressureP.classList.add("todayHighlightsTitle");
+  cardPressureP.textContent = "Pressure:";
+  const cardPressureValue = document.createElement("p");
+  cardPressureValue.classList.add("todayHighlightsValue");
+  cardPressure.appendChild(cardPressureP);
+  cardPressure.appendChild(cardPressureValue);
+  todayHighlightsDIV.appendChild(cardPressure);
+
+  // Wind
+  const cardWind = document.createElement("div");
+  cardWind.classList.add("todayHighlightsCard");
+  const cardWindP = document.createElement("p");
+  cardWindP.classList.add("todayHighlightsTitle");
+  cardWindP.textContent = "Wind:";
+  const cardWindValue = document.createElement("p");
+  cardWindValue.classList.add("todayHighlightsValue");
+  cardWind.appendChild(cardWindP);
+  cardWind.appendChild(cardWindValue);
+  todayHighlightsDIV.appendChild(cardWind);
+
+  // Air quality:
+  const cardAirQuality = document.createElement("div");
+  cardAirQuality.classList.add("todayHighlightsCard");
+  const cardAirQualityP = document.createElement("p");
+  cardAirQualityP.classList.add("todayHighlightsTitle");
+  cardAirQualityP.textContent = "Air quality (US EPA):";
+  const cardAirQualityValue = document.createElement("p");
+  cardAirQualityValue.classList.add("todayHighlightsValue");
+  cardAirQuality.appendChild(cardAirQualityP);
+  cardAirQuality.appendChild(cardAirQualityValue);
+  todayHighlightsDIV.appendChild(cardAirQuality);
+
+  // Visibility:
+  const cardVisibility = document.createElement("div");
+  cardVisibility.classList.add("todayHighlightsCard");
+  const cardVisibilityP = document.createElement("p");
+  cardVisibilityP.classList.add("todayHighlightsTitle");
+  cardVisibilityP.textContent = "Visibility:";
+  const cardVisibilityValue = document.createElement("p");
+  cardVisibilityValue.classList.add("todayHighlightsValue");
+  cardVisibility.appendChild(cardVisibilityP);
+  cardVisibility.appendChild(cardVisibilityValue);
+  todayHighlightsDIV.appendChild(cardVisibility);
+
   rightDOM.appendChild(weekDIV);
+
+  todayHighlightsP.textContent = "Current weather"; // permanent so add here
+  rightDOM.appendChild(todayHighlightsP);
+
+  rightDOM.appendChild(todayHighlightsDIV);
 
   return {
     locationP,
@@ -98,22 +185,14 @@ export function addRightPartDOM() {
     card3TextDay,
     card3TextTemp,
     card3WeatherImg,
+    cardUVvalue,
+    sunscreenReminder,
+    feelsLikeValue,
+    cardPressureValue,
+    cardWindValue,
+    cardAirQualityValue,
+    cardVisibilityValue,
   };
-}
-
-export async function setLeftPartDOMContent() {
-  const {
-    locationP,
-    card1TextDay,
-    card1TextTemp,
-    card1WeatherImg,
-    card2TextDay,
-    card2TextTemp,
-    card2WeatherImg,
-    card3TextDay,
-    card3TextTemp,
-    card3WeatherImg,
-  } = addRightPartDOM();
 }
 
 export async function setRightPartDOMContent(cityInput) {
@@ -135,6 +214,13 @@ export async function setRightPartDOMContent(cityInput) {
     card3TextDay,
     card3TextTemp,
     card3WeatherImg,
+    cardUVvalue,
+    sunscreenReminder,
+    feelsLikeValue,
+    cardPressureValue,
+    cardWindValue,
+    cardAirQualityValue,
+    cardVisibilityValue,
   } = elements;
 
   const {
@@ -152,6 +238,18 @@ export async function setRightPartDOMContent(cityInput) {
     conditionCodeCard3,
   } = await fetchNextThreeDaysData(cityInput);
 
+  const {
+    weatherConditionID,
+    localTimeHour,
+    uvIndex,
+    feelLike,
+    wind,
+    visibility,
+    airQuality,
+    pressure,
+    windDirection,
+  } = await fetchCurrentData(cityInput);
+
   locationP.textContent = city + ", " + country;
 
   card1TextDay.textContent = tomorrow + ", ";
@@ -168,9 +266,73 @@ export async function setRightPartDOMContent(cityInput) {
   card3TextTemp.textContent = `${maxTempCard3} °C / ${minTempCard3} °C`;
   const imgSrc3 = await addImage(conditionCodeCard3, timeHourCardAll);
   card3WeatherImg.src = imgSrc3;
+
+  const { uvText, suncreenReminde } = UVrate(uvIndex);
+  cardUVvalue.textContent = uvIndex + " - " + uvText;
+  sunscreenReminder.textContent = suncreenReminde;
+
+  feelsLikeValue.textContent = `${feelLike} °C`;
+
+  cardPressureValue.textContent = pressure + " mb";
+
+  cardWindValue.textContent = wind + " (" + windDirection + ")";
+
+  const { airQualityText } = AQI(airQuality);
+  cardAirQualityValue.textContent = airQuality + " - " + airQualityText;
+
+  cardVisibilityValue.textContent = visibility + " km";
 }
 
 export function cleanRightDOM() {
   const rightDOM = document.querySelector(".right");
   rightDOM.innerHTML = "";
+}
+
+function UVrate(uvValue) {
+  if (uvValue < 3) {
+    const uvText = "low";
+    const suncreenReminde = "No protection needed";
+    return { uvText, suncreenReminde };
+  } else if (uvValue >= 3 && uvValue < 6) {
+    const uvText = "moderate";
+    const suncreenReminde = "Some protection is required";
+    return { uvText, suncreenReminde };
+  } else if (uvValue >= 6 && uvValue < 8) {
+    const uvText = "high";
+    const suncreenReminde = "Protection essential";
+    return { uvText, suncreenReminde };
+  } else if (uvValue >= 8 && uvValue < 11) {
+    const uvText = "very high";
+    const suncreenReminde = "Extra protection is needed";
+    return { uvText, suncreenReminde };
+  } else if (uvValue >= 11) {
+    const uvText = "extreme";
+    const suncreenReminde = "Stay inside";
+    return { uvText, suncreenReminde };
+  }
+
+  // Default return if none of the conditions are met
+  const uvText = "unknown";
+  const suncreenReminde = "Unknown protection level";
+  return { uvText, suncreenReminde };
+}
+
+function AQI(airQualityValue) {
+  if (airQualityValue < 51) {
+    const airQualityText = "good";
+    return { airQualityText };
+  } else if (airQualityValue >= 51 && airQualityValue < 101) {
+    const airQualityText = "moderate";
+    return { airQualityText };
+  } else if (airQualityValue >= 101 && airQualityValue < 151) {
+    const airQualityText = "unhealthy for sensitive groups";
+    return { airQualityText };
+  } else if (airQualityValue >= 151) {
+    const airQualityText = "unhealthy";
+    return { airQualityText };
+  }
+
+  // Default return if none of the conditions are met
+  const airQualityText = "unknown";
+  return { airQualityText };
 }
